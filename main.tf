530c16937e75329a4c48129c298ee0b3721ff4d9
manoj515876 <manojputtaluckey@gmail.com>
2026-01-30 14:46:05 +0530

    AWS VPC module

---
+++ main.tf
@@ -0,0 +1,13 @@
+resource "aws_vpc" "main" {
+  cidr_block       = var.cidr_block
+  instance_tenancy = "default"
+  enable_dns_hostnames = var.enable_dns_hostnames
+
+  tags = merge(
+    var.common_tags,
+    var.vpc_tags,
+    {
+        Name = "${var.project_name}-${var.environment}"
+    }
+  )
+}
---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "cidr_block" {
+
+}
+
+variable "enable_dns_hostnames" {
+  default = "true"
+}
+
+variable "common_tags" {
+    
+}
+
+variable "project_name" {
+
+}
+
+variable "environment" {
+
+}
+
+variable "vpc_tags" {
+    
+}
