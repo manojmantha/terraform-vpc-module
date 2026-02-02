c95934821169ff495afcb0db6641846a30c018c4
manoj515876 <manojputtaluckey@gmail.com>
2026-02-02 14:27:41 +0530

    VPC and VPC Peering module

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "main" {
-  cidr_block       = var.cidr_block
+  cidr_block       = var.vpc_cidr
   instance_tenancy = "default"
   enable_dns_hostnames = var.enable_dns_hostnames
 
